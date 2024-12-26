6318acb2be5cfeaa94e981e0cace12844360aca1
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-12-26 16:20:18 +0530

    first commit

--- Route/Instructor.js
+++ Route/Instructor.js
@@ -1,13 +1,9 @@
 const Router = require("express").Router();
 const { InstructorPost, InstructorGet, InstructorUpdate, InstructorIdDelete ,InstructorGetId } = require("../Controller/InstructorController");
 
-
-
 Router.post("/instrutor_post", InstructorPost)
 
 Router.get("/instrutor_get/:Id", InstructorGetId)
-
-
 
 Router.get("/instrutor_get", InstructorGet)
 
